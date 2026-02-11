ce2f031cb8a3bfbb102ae81c32dddae14b211228
programmerShinobi <shinobiprogrammer@gmail.com>
2026-02-11 11:06:55 +0700

    [add] learn : configuration properties

--- src/main/java/programmer_shinobi/belajar_spiring_validation/BelajarSpiringValidationApplication.java
+++ src/main/java/programmer_shinobi/belajar_spiring_validation/BelajarSpiringValidationApplication.java
@@ -2,8 +2,13 @@ package programmer_shinobi.belajar_spiring_validation;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import programmer_shinobi.belajar_spiring_validation.properties.DatabaseProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties({
+		DatabaseProperties.class
+})
 public class BelajarSpiringValidationApplication {
 
 	public static void main(String[] args) {
